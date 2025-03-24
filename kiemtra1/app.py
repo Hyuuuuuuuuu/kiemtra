efdbd0f89999dba97c1d2249d4ffbf9ad58ab0ea
PhungDucTin <phungductin280504@gmail.com>
2025-03-24 10:52:14 +0700

    [add] kiemtra1

---
+++ kiemtra1/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from Vigenere import VigenereCipher
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+# ------------------ VIGENERE CIPHER ------------------
+@app.route("/vigenere")
+def vigenere():
+    return render_template('vigenere.html')
+
+@app.route("/vigenere/encrypt", methods=['POST'])
+def vigenere_encrypt():
+    text = request.form['inputPlainText']
+    key = request.form['inputKeyPlain']
+    Vigenere = VigenereCipher()
+    encrypted_text = Vigenere.vigenere_encrypted(text, key)
+    return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
+
+@app.route("/vigenere/decrypt", methods=['POST'])
+def vigenere_decrypt():
+    text = request.form['inputCipherText']
+    key = request.form['inputKeyCipher']
+    Vigenere  = VigenereCipher()
+    decrypted_text = Vigenere.vigenere_decrypted(text, key)
+    return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050, debug=True)
